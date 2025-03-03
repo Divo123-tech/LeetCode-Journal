231483f98b24dc63be98ef35ade2e30902941d14
Cristopher Harsono <67683497+Divo123-tech@users.noreply.github.com>
2025-03-03 20:25:27 +0800

    Update ransomNote.ts

--- ransomNote.ts
+++ ransomNote.ts
@@ -1,1 +1,24 @@
+function canConstruct(ransomNote: string, magazine: string): boolean {
+    let lettersInMag = {}
+    for(const letter of magazine){
+        if(lettersInMag[letter]){
+            lettersInMag[letter]++
+        }
+        else{
+            lettersInMag[letter] = 1
+        }
+    }
 
+    for(const letter of ransomNote){
+        if(lettersInMag[letter]){
+            lettersInMag[letter]--
+        }
+        else if(lettersInMag[letter] == 0){
+            return false
+        }
+        else{
+            return false
+        }
+    }
+    return true
+};
