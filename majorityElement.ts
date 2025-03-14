99716fc25912017c6f485861523ae095c5ef7c45
Cristopher Harsono <67683497+Divo123-tech@users.noreply.github.com>
2025-03-14 15:37:41 +0800

    Update majorityElement.ts

--- majorityElement.ts
+++ majorityElement.ts
@@ -1,1 +1,17 @@
-
+function majorityElement(nums: number[]): number {
+    let nums_dict = {}
+    if(nums.length == 1){
+        return nums[0]
+    }
+    for (const num of nums){
+        if(nums_dict[num]){
+            nums_dict[num]++
+            if(nums_dict[num] > nums.length/2){
+                return num
+            }
+        }
+        else{
+            nums_dict[num] = 1
+        }
+    }
+};
