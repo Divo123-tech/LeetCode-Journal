804ea954c4384b91323afb783416c84e5556d44c
Cristopher Harsono <67683497+Divo123-tech@users.noreply.github.com>
2025-03-21 16:23:38 +0800

    Update diameterofABinaryTree.ts

--- diameterofABinaryTree.ts
+++ diameterofABinaryTree.ts
@@ -1,1 +1,35 @@
 
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function diameterOfBinaryTree(root: TreeNode | null): number {
+    let longestTotal = 0
+    function dfs(root){
+    if (root == null){
+        return 0;
+    }
+
+    
+    const leftDepth = dfs(root.left)        
+    const rightDepth = dfs(root.right)     
+    longestTotal = Math.max(longestTotal, leftDepth + rightDepth)
+    const longest = Math.max(leftDepth, rightDepth)
+    return longest + 1 
+}
+    dfs(root)
+    return longestTotal
+
+
+
+};
