9f308b8573eceee6d06918799341bd3668d5f9a4
Cristopher Harsono <67683497+Divo123-tech@users.noreply.github.com>
2025-03-22 15:42:37 +0800

    Update diameterofABinaryTree.ts

--- diameterofABinaryTree.ts
+++ diameterofABinaryTree.ts
@@ -22,11 +22,15 @@ function diameterOfBinaryTree(root: TreeNode | null): number {
         return 0;
     }
 
-    
+    //check depth of left and right subtree
     const leftDepth = dfs(root.left)        
     const rightDepth = dfs(root.right)     
+    //assign the longest total based on whether the current Longest Total 
+    //or the current diamter of the subtree is bigger
     longestTotal = Math.max(longestTotal, leftDepth + rightDepth)
+    //return the longest tree between left and right
     const longest = Math.max(leftDepth, rightDepth)
+    //return longest + 1 to account for trees of size 1
     return longest + 1 
 }
     dfs(root)
