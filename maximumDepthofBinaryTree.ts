bb7f02412f70d1d2cd2214b5fe126ab4d628a94b
Cristopher Harsono <67683497+Divo123-tech@users.noreply.github.com>
2025-03-25 14:49:35 +0800

    Update maximumDepthofBinaryTree.ts

--- maximumDepthofBinaryTree.ts
+++ maximumDepthofBinaryTree.ts
@@ -1,1 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
 
+function maxDepth(root: TreeNode | null): number {
+    let longestDepth = 1;
+    if (root == null){
+        return 0;
+    }
+    function dfs(root){
+        if (root == null){
+            return 0
+        }
+        const left = dfs(root.left)
+        const right = dfs(root.right)
+        const longest = Math.max(left, right) + 1
+        if (longest > longestDepth){
+            longestDepth = longest
+        }
+        return longest
+    }
+    return dfs(root)
+};
