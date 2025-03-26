b5db00d8374424fe1abed2a9883605f3f42cc3e2
Cristopher Harsono <67683497+Divo123-tech@users.noreply.github.com>
2025-03-26 18:39:31 +0800

    Update romanToInteger.ts

--- romanToInteger.ts
+++ romanToInteger.ts
@@ -1,1 +1,25 @@
+function romanToInt(s: string): number {
+    const romanIntMap = {
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
+    }
+    let total = 0;
+    let lastValue = 0;
+    for(let i = s.length - 1; i >= 0; i--){
+        const letter = s[i]
+        if (lastValue > romanIntMap[letter] ){
+            total -= romanIntMap[letter]
+        }
+        else{
+            total += romanIntMap[letter]
+        }
+        lastValue = romanIntMap[letter]
 
+    }
+    return total
+};
