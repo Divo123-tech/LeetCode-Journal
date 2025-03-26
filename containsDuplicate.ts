1ff1517b6f3914a2662b6e01d5b5de96bed36b4a
Cristopher Harsono <67683497+Divo123-tech@users.noreply.github.com>
2025-03-26 18:23:42 +0800

    Update containsDuplicate.ts

--- containsDuplicate.ts
+++ containsDuplicate.ts
@@ -1,1 +1,10 @@
-
+function containsDuplicate(nums: number[]): boolean {
+    const nums_dict = {}
+    for(const num of nums){
+        if (nums_dict[num]){
+            return true
+        }
+        nums_dict[num] = 1
+    }
+    return false
+};
