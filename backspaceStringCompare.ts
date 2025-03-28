f3a27807c0f1b80a19cd306a079d123666d2ac09
Cristopher Harsono <67683497+Divo123-tech@users.noreply.github.com>
2025-03-28 12:53:20 +0800

    Update backspaceStringCompare.ts

--- backspaceStringCompare.ts
+++ backspaceStringCompare.ts
@@ -1,1 +1,21 @@
-
+function backspaceCompare(s: string, t: string): boolean {
+    let sModified = "";
+    for (let i = 0; i < s.length; i++){
+        if (s[i] == "#"){
+            sModified = sModified.substring(0, sModified.length - 1);
+        }
+        else{
+            sModified += s[i];
+        }
+    }
+    let tModified = "";
+     for (let i = 0; i < t.length; i++){
+        if (t[i] == "#"){
+            tModified = tModified.substring(0, tModified.length - 1);
+        }
+        else{
+            tModified += t[i];
+        }
+    }
+    return sModified == tModified
+};
